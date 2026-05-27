85d614b3d390d2c604b9cb360158bd9a3e5a27bc
akabel <akabel@spungegames.com>
2026-05-27 21:08:13 +1000

    Adding camera movement with mouse when looking up

--- player_hike.gd
+++ player_hike.gd
@@ -1,11 +1,13 @@
 extends CharacterBody3D
-var facingUP: bool = true
+var facingUp: bool = true
 
 var upAngle = deg_to_rad(0)
 var downAngle = deg_to_rad(-50)
+var forwardFacing = deg_to_rad(0)
 
+var lookDir: Vector2
 @onready var camera: Camera3D = $Camera3D
-
+var cameraSens = 50
 	
 func _physics_process(delta):
 	var cameraRotationSpeed: float = 5
@@ -13,16 +15,28 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("look_up"):
 		$Camera3D.rotation.x = lerp_angle($Camera3D.rotation.x, upAngle, 1) 
 		print($Camera3D.rotation_degrees.x) 
-		var facingUP: bool = true
-		print(facingUP) 
+		facingUp = true
+		print(facingUp) 
+		print("can rotate")
 		
 	if Input.is_action_just_pressed("look_down"):
 		$Camera3D.rotation.x = lerp_angle($Camera3D.rotation.x, downAngle, 1) 
+		$Camera3D.rotation.y = forwardFacing
 		#lerp_angle($CameraPivot.rotation_degrees.x, downAngle, delta*cameraRotationSpeed)
 		print($Camera3D.rotation_degrees.x) 
-		var facingUP: bool = false
-		print(facingUP)
+		facingUp = false
+		print(facingUp)
+		
+	if facingUp == true:
+		_rotate_camera(delta)
 		
 		
-
+func _input(event: InputEvent):
+	if facingUp == true:
+		if event is InputEventMouseMotion: lookDir = event.relative*0.01
 	
+func _rotate_camera(delta: float, sens_mod: float = 1.0):
+	var input = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	lookDir += input
+	rotation.y -= lookDir.x * cameraSens * delta
+	lookDir = Vector2.ZERO
