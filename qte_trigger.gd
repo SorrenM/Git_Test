67b3e969e50f7ad478a518be29fab72d6f80d376
akabel <akabel@spungegames.com>
2026-06-10 20:00:04 +1000

    Plugin added: onscreen output
    Added a plugin to easily print debug to the screen instead of just the output log

--- qte_trigger.gd
+++ qte_trigger.gd
@@ -11,19 +11,19 @@ func _on_body_entered(body):
 	if body.is_in_group("player"):
 		if self.is_in_group("leftFoot"):
 			Global.inputAction = "left_foot_dodge"
-			print("QTE = ", Global.inputAction)
+			Output.print("QTE = "+ Global.inputAction)
 			
 		if self.is_in_group("rightFoot"):
 			Global.inputAction = "right_foot_dodge"
-			print("QTE = ", Global.inputAction)
+			Output.print("QTE = "+ Global.inputAction)
 				
 		if self.is_in_group("leftHead"):
 			Global.inputAction = "left_head_dodge"
-			print("QTE = ", Global.inputAction)
+			Output.print("QTE = "+ Global.inputAction)
 				
 		if self.is_in_group("rightHead"):
 			Global.inputAction = "right_head_dodge"
-			print("QTE = ", Global.inputAction)
+			Output.print("QTE = "+ Global.inputAction)
 		QTE.start_qte()
 		print("Timer started")
 		#print("QTE started, timer = ",Global.dodgeTime)
